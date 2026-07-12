8cf1700004476fd4c87064e8f7dc964429e96177
alona-s0 <alona.sol.25@gmail.com>
2026-07-12 15:07:22 +0100

    Added the AI student impact dataset

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from dash import Dash, html
+from dash import Dash, html, dcc, callback, Output, Input
+import dash_ag_grid as dag
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("ai_student_impact_dataset.csv")
 
 app = Dash()
 
-app.layout = [html.Div(children='Hello World')]
+app.layout = [
+    html.Div(children='AI impact on students'),
+    dag.AgGrid(
+        rowData=df.to_dict('records'),
+        columnDefs=[{"field": i} for i in df.columns]
+    )
+    ]
 
 if __name__ == '__main__':
     app.run(debug=True)
