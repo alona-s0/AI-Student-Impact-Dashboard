e64315c37d034d34025180bc4917451ce7a8bb4e
alona-s0 <alona.sol.25@gmail.com>
2026-07-12 16:18:25 +0100

    Added a scatter plot visualization with industry filter

--- app.py
+++ app.py
@@ -9,11 +9,29 @@ app = Dash()
 
 app.layout = [
     html.Div(children='AI impact on students'),
+    html.Hr(),
     dag.AgGrid(
         rowData=df.to_dict('records'),
         columnDefs=[{"field": i} for i in df.columns]
-    )
-    ]
+    ),
+    dcc.RadioItems(options=['Humanities', 'Medical', 'Business', 'STEM', 'Arts'], value='Humanities', id='majors-radio-item'),
+    dcc.Graph(figure={}, id='scatter-graph1')
+
+]
+
+@callback(
+    Output(component_id='scatter-graph1', component_property='figure'),
+    Input(component_id='majors-radio-item', component_property='value')
+)
+
+def update_graph(major):
+    if major == "All":
+        filtered_df = df
+    else:
+        filtered_df = df[df["Major_Category"]==major]
+
+    scatter_fig1 = px.scatter(filtered_df, x='Post_Semester_GPA', y='Weekly_GenAI_Hours')
+    return scatter_fig1
 
 if __name__ == '__main__':
     app.run(debug=True)
