ba8a728cb092bd56583d58a763aade53c2bf6b8a
alona-s0 <alona.sol.25@gmail.com>
2026-07-13 12:58:20 +0100

    Cleaned up the dashboard code

--- app.py
+++ app.py
@@ -8,38 +8,42 @@ df = pd.read_csv("ai_student_impact_dataset.csv")
 app = Dash()
 
 app.layout = [
-    html.Div(children='AI impact on students'),
+    html.H1('AI impact on students', style={'textAlign':'center'}),
     html.Hr(),
     dag.AgGrid(
         rowData=df.to_dict('records'),
         columnDefs=[{"field": i} for i in df.columns]
     ),
 
-    dcc.RadioItems(options=['Weekly_GenAI_Hours', 'Traditional_Study_Hours', 'Anxiety_Level_During_Exams'], value='Weekly_GenAI_Hours', id='general-info-radio-item'),
-    dcc.Graph(figure={}, id='histogram-graph1'),
+    html.H2("Time spent on Gen AI per week by each major"),
+    dcc.RadioItems(options=['Weekly_GenAI_Hours', 'Traditional_Study_Hours', 'Anxiety_Level_During_Exams'], value='Weekly_GenAI_Hours', inline=True, id='general-info-radio-item'),
+    dcc.Graph(figure={}, id='general-info-histogram'),
     html.Hr(),
 
+    html.H2("The impact of Gen AI on grades"),
     dcc.RadioItems(options=['Humanities', 'Medical', 'Business', 'STEM', 'Arts'], value='Humanities', inline=True, id='majors-radio-item'),
-    dcc.Graph(figure={}, id='scatter-graph1'),
+    dcc.Graph(figure={}, id='GPA_Scatter'),
     html.Hr(),
 
+    html.H2("Exploring the effects of AI usage on learning metrics"), #for the last graph
     dcc.Dropdown(options=['All', 'Humanities', 'Medical', 'Business', 'STEM', 'Arts'], value='All', id='majors-dropdown'),
-    dcc.Graph(figure={}, id='heatmap-graph1'),
-    dcc.Graph(figure={}, id='heatmap-graph2'),
+    dcc.Graph(figure={}, id='Skill_Retention_Heatmap'),
+    dcc.Graph(figure={}, id='AI_Dependency_Heatmap'),
     html.Hr(),
 
+    html.H2("Does AI affect stress levels during exams?"),
     dcc.Dropdown(options=['All', 'Humanities', 'Medical', 'Business', 'STEM', 'Arts'], value='All', id='majors-dropdown2'),
-    dcc.Graph(figure={}, id='box-graph1'),
-    dcc.Graph(figure={}, id='box-graph2')
+    dcc.Graph(figure={}, id='AI_Anxiety_Levels_Box'),
+    dcc.Graph(figure={}, id='Trad_Anxiety_Levels_Box')
 ]
 
 @callback(
-    Output(component_id='histogram-graph1', component_property='figure'),
-    Output(component_id='scatter-graph1', component_property='figure'),
-    Output(component_id='heatmap-graph1', component_property='figure'),
-    Output(component_id='heatmap-graph2', component_property='figure'),
-    Output(component_id='box-graph1', component_property='figure'),
-    Output(component_id='box-graph2', component_property='figure'),
+    Output(component_id='general-info-histogram', component_property='figure'),
+    Output(component_id='GPA_Scatter', component_property='figure'),
+    Output(component_id='Skill_Retention_Heatmap', component_property='figure'),
+    Output(component_id='AI_Dependency_Heatmap', component_property='figure'),
+    Output(component_id='AI_Anxiety_Levels_Box', component_property='figure'),
+    Output(component_id='Trad_Anxiety_Levels_Box', component_property='figure'),
     Input(component_id='general-info-radio-item', component_property='value'),
     Input(component_id='majors-radio-item', component_property='value'),
     Input(component_id='majors-dropdown', component_property='value'),
@@ -47,33 +51,26 @@ app.layout = [
 )
 
 def update_graph(col_chosen, radio_major, dropdown_major, dropdown_major2):
-    hist_fig1 = px.histogram(df, x='Major_Category', y=col_chosen, histfunc='avg')
-
-    if radio_major == "All":
-        filtered_df = df
-    else:
-        filtered_df = df[df["Major_Category"]==radio_major]
-
-    scatter_fig1 = px.scatter(filtered_df, x='Post_Semester_GPA', y='Weekly_GenAI_Hours')
-
-    if dropdown_major == "All":
-        filtered_df_heatmap = df
-    else:
-        filtered_df_heatmap= df[df["Major_Category"]==dropdown_major]
-
-    heatmap_fig1 = px.density_heatmap(filtered_df_heatmap, x='Skill_Retention_Score', y='Weekly_GenAI_Hours')
-
-    heatmap_fig2 = px.density_heatmap(filtered_df_heatmap, x='Perceived_AI_Dependency', y='Weekly_GenAI_Hours')
-
-    if dropdown_major2 == "All":
-        filtered_df_box = df
-    else:
-        filtered_df_box = df[df["Major_Category"]==dropdown_major2]
-
-    box_fig1 = px.box(filtered_df_box, x='Anxiety_Level_During_Exams', y='Weekly_GenAI_Hours', points ='outliers')
-    box_fig2 = px.box(filtered_df_box, x='Anxiety_Level_During_Exams', y='Traditional_Study_Hours', points ='outliers')
+    def filter_major(major):
+        if major == "All":
+            return df
+        return df[df["Major_Category"]==major]
     
-    return hist_fig1, scatter_fig1, heatmap_fig1, heatmap_fig2, box_fig1, box_fig2
+    major_histogram = px.histogram(df, x='Major_Category', y=col_chosen, histfunc='avg')
+
+    filtered_df = filter_major(radio_major)
+    gpa_scatter = px.scatter(filtered_df, x='Post_Semester_GPA', y='Weekly_GenAI_Hours')
+
+    filtered_df_heatmap = filter_major(dropdown_major)
+    skill_heatmap = px.density_heatmap(filtered_df_heatmap, x='Skill_Retention_Score', y='Weekly_GenAI_Hours')
+
+    ai_heatmap = px.density_heatmap(filtered_df_heatmap, x='Perceived_AI_Dependency', y='Weekly_GenAI_Hours')
+
+    filtered_df_box = filter_major(dropdown_major2)
+    ai_anxiety_box = px.box(filtered_df_box, x='Anxiety_Level_During_Exams', y='Weekly_GenAI_Hours', points ='outliers')
+    trad_anxiety_box = px.box(filtered_df_box, x='Anxiety_Level_During_Exams', y='Traditional_Study_Hours', points ='outliers')
+    
+    return major_histogram, gpa_scatter, skill_heatmap, ai_heatmap, ai_anxiety_box, trad_anxiety_box
 
 if __name__ == '__main__':
     app.run(debug=True)
